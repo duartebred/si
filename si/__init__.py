2c20215e8df6a6dc557710e24c6cfeea704a2668
duartebred <duartealvesvelho@gmai.com>
2025-01-03 20:25:01 +0000

    [ADD] __init__.py

---
+++ si/__init__.py
@@ -0,0 +1,6 @@
+__author__ = "Duarte Velho"
+__credits__ = ["Duarte Velho"]
+__license__ = "Apache License 2.0"
+__version__ = "0.0.1"
+__maintainer__ = "Duarte Velho"
+__email__ = "duartealvesvelho@gmail.com"
